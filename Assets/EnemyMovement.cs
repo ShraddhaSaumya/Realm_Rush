d82bfdec68f090548d4c8455c66fa30878879df2
ShraddhaSaumya <60255492+ShraddhaSaumya@users.noreply.github.com>
2020-04-16 15:44:34 +0530

    till C# lists

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        printAllWayPoints();   
+    }
+
+    private void printAllWayPoints()
+    {
+        foreach (Block wayPoint in path)
+            print(wayPoint.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
